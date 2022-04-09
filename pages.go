d7883e4beb5b3f777cf6d5874312de69c712dc5f
Stacey Brogden <toad@brothertoad.net>
2022-04-09 09:39:30 -0700

    Break out page logic into its own file.

---
+++ pages.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strings"
+  "io/fs"
+  "io/ioutil"
+  "path/filepath"
+)
+
+func processPages() {
+  // Need to verify pages directory exists
+  err := filepath.Walk(pageDir, func(path string, fileInfo fs.FileInfo, err error) error {
+    // Ignore non-html files.
+    if filepath.Ext(path) != ".html" {
+      return nil
+    }
+    // Ignore files with a leading underscore, except _index.html.
+    if strings.HasPrefix(fileInfo.Name(), "_")  && fileInfo.Name() != "_index.html" {
+      return nil
+    }
+    // Need to generate the output path, which mirrors the source path.
+    info := buildPageInfo(path, fileInfo)
+    b, ferr := ioutil.ReadFile(path)
+    checkError(ferr)
+    // Clone the layout template, so we don't add have residue from previous pages.
+    t, terr := layoutTemplate.Clone()
+    checkError(terr)
+    t, terr = t.Parse(string(b))
+    checkError(terr)
+    // Now we can execute the template and write the output.
+    fmt.Printf("Output will be written to %s (may need to create directory)\n", info.outputPath)
+    return nil
+  })
+  checkError(err)
+}
+
+func buildPageInfo(path string, fileInfo fs.FileInfo) pageInfo {
+  var info pageInfo
+  dir, base := filepath.Split(path)
+  fmt.Printf("path is %s, dir is %s, base is %s\n", path, dir, base)
+  parts := strings.Split(path, string(os.PathSeparator))
+  // TASK: handle the case where pageDir and/or outputDir has multiple components
+  parts[0] = outputDir
+  // If the filename is _index.html, change it to index.html.  Otherwise, remove the
+  // .html and add a separator followed by index.html.
+  if base == "_index.html" {
+    parts[len(parts)-1] = "index.html"
+  } else {
+    lastDir := strings.TrimSuffix(base, ".html")
+    parts[len(parts)-1] = lastDir
+    parts = append(parts, "index.html")
+  }
+  info.outputPath = filepath.Join(parts...)
+  info.dataPath = strings.TrimSuffix(path, ".html") + ".yaml"
+  fmt.Printf("Output path is %s, data path is %s\n", info.outputPath, info.dataPath)
+  return info
+}

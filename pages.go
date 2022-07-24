444cc65f6bee865339a5e29834788522fa32ed7e
Stacey Brogden <toad@brothertoad.net>
2022-07-24 13:00:16 -0700

    Support .html, html.j2 and .j2 extersions for files.

--- pages.go
+++ pages.go
@@ -24,12 +24,8 @@ type pageInfo struct {
 func processPages(pageDir string, outputDir string, globalData map[string]interface{}) {
   btu.DirMustExist(pageDir)
   err := filepath.Walk(pageDir, func(path string, fileInfo fs.FileInfo, err error) error {
-    // Ignore non-html files.
-    if filepath.Ext(path) != ".html" {
-      return nil
-    }
-    // Ignore files with a leading underscore, except _index.html.
-    if strings.HasPrefix(fileInfo.Name(), "_")  && fileInfo.Name() != "_index.html" {
+    // Check the extension and filename to see if we should ignore this file.
+    if !validExtension(fileInfo.Name()) || !validFilename(fileInfo.Name()) {
       return nil
     }
 
@@ -97,22 +93,57 @@ func processPages(pageDir string, outputDir string, globalData map[string]interf
   btu.CheckError(err)
 }
 
+func validExtension(filename string) bool {
+  if strings.HasSuffix(filename, ".html") {
+    return true
+  }
+  // Note that this case also covers .html.j2
+  if strings.HasSuffix(filename, ".j2") {
+    return true
+  }
+  return false
+}
+
+func validFilename(filename string) bool {
+  if !strings.HasPrefix(filename, "_") {
+    return true
+  }
+  if filename == "_index.html" || filename == "_index.html.j2" || filename == "_index.j2" {
+    return true
+  }
+  return false
+}
+
 func buildPageInfo(path string, outputDir string, fileInfo fs.FileInfo) pageInfo {
   var info pageInfo
   _, base := filepath.Split(path)
   parts := strings.Split(path, string(os.PathSeparator))
+  suffix := getSuffix(base)
   // TASK: handle the case where pageDir and/or outputDir has multiple components
   parts[0] = outputDir
-  // If the filename is _index.html, change it to index.html.  Otherwise, remove the
-  // .html and add a separator followed by index.html.
-  if base == "_index.html" {
+  // If the filename is _index.html or one of its variants, change it to index.html.
+  // Otherwise, remove the suffix and add a separator followed by index.html.
+  if base == "_index.html" || base == "_index.html.j2" || base == "_index.j2" {
     parts[len(parts)-1] = "index.html"
   } else {
-    lastDir := strings.TrimSuffix(base, ".html")
-    parts[len(parts)-1] = lastDir
+    parts[len(parts)-1] = strings.TrimSuffix(base, suffix)
     parts = append(parts, "index.html")
   }
   info.outputPath = filepath.Join(parts...)
-  info.dataPath = strings.TrimSuffix(path, ".html") + ".yaml"
+  info.dataPath = strings.TrimSuffix(path, suffix) + ".yaml"
   return info
+}
+
+func getSuffix(base string) string {
+  if strings.HasSuffix(base, ".html") {
+    return ".html"
+  }
+  if strings.HasSuffix(base, ".html.j2") {
+    return ".html.j2"
+  }
+  if strings.HasSuffix(base, ".j2") {
+    return ".j2"
+  }
+  log.Fatalf("Can't get suffix of %s\n", base)
+  return ""
 }

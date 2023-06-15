95ad2216fe0b2ff052eaecd6fda25c475f36c965
Stacey Brogden <toad@brothertoad.net>
2023-06-15 11:21:31 -0700

    Start work on uuadd.

--- cmd/uuadd/uuadd.go
+++ cmd/uuadd/uuadd.go
@@ -2,8 +2,29 @@ package main
 
 import (
   "fmt"
+  "os"
+  "path/filepath"
+  "strings"
+  "github.com/brothertoad/goss/gossutil"
 )
 
+const DATA_DIR = "data"
+const PAGES_DIR = "pages"
+const SRC_DIR = "pages/models"
+const J2_SUFFIX = "html.j2"
+const YAML_SUFFIX = "yaml"
+const PER_PAGE_DIR = "perPage"
+
+var globalData map[string]interface{}
+
 func main() {
-  fmt.Printf("uuad is running...\n")
+  globalData = gossutil.LoadGlobalData(DATA_DIR)
+  _ = filepath.Walk(SRC_DIR, func(path string, info os.FileInfo, err error) error {
+    if strings.HasSuffix(path, J2_SUFFIX) {
+      relativePath := path[(len(PAGES_DIR) + 1):]
+      dataRelativePath := filepath.Join(PER_PAGE_DIR, (strings.TrimSuffix(relativePath, J2_SUFFIX) + YAML_SUFFIX))
+      fmt.Printf("Walking %s, relativePath is %s, dataRelativePath is %s\n", path, relativePath, dataRelativePath)
+    }
+    return nil
+  })
 }
